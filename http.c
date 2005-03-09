14d900a2dfe7c3f665a794d7cf75fb4053f83dfb
Michael Wallner <mike@php.net>
2005-03-09 13:23:13 +0000

    * add webdav request methods to standard allowed methods

--- http.c
+++ http.c
@@ -417,7 +417,9 @@ PHP_INI_MH(update_allowed_methods)
 }
 
 PHP_INI_BEGIN()
-	STD_PHP_INI_ENTRY("http.allowed_methods", "OPTIONS,GET,HEAD,POST,PUT,DELETE,TRACE,CONNECT", PHP_INI_ALL, update_allowed_methods, allowed_methods, zend_http_globals, http_globals)
+	STD_PHP_INI_ENTRY("http.allowed_methods", 
+		"OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT, PROPFIND, PROPPATCH, MKCOL, COPY, MOVE, LOCK, UNLOCK", 
+		PHP_INI_ALL, update_allowed_methods, allowed_methods, zend_http_globals, http_globals)
 PHP_INI_END()
 /* }}} */
 
