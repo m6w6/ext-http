8d9da7c8014b2738ec646b0415223f75f43059e7
Michael Wallner <mike@php.net>
2005-05-30 12:06:47 +0000

    - define zend_(upate|declare)_property_double only for PHP-5.0

--- missing.c
+++ missing.c
@@ -1,7 +1,24 @@
+/*
+   +----------------------------------------------------------------------+
+   | PECL :: http                                                         |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.0 of the PHP license, that  |
+   | is bundled with this package in the file LICENSE, and is available   |
+   | through the world-wide-web at http://www.php.net/license/3_0.txt.    |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2004-2005 Michael Wallner <mike@php.net>               |
+   +----------------------------------------------------------------------+
+*/
+
+/* $Id$ */
 
 #include "php.h"
 #include "missing.h"
 
+#if (PHP_MAJOR_VERSION == 5) && (PHP_MINOR_VERSION == 0)
 int zend_declare_property_double(zend_class_entry *ce, char *name, int name_length, double value, int access_type TSRMLS_DC)
 {
 	zval *property;
@@ -26,6 +43,8 @@ void zend_update_property_double(zend_class_entry *scope, zval *object, char *na
 	ZVAL_DOUBLE(tmp, value);
 	zend_update_property(scope, object, name, name_length, tmp TSRMLS_CC);
 }
+
+#endif
 
 /*
  * Local variables:
--- missing.h
+++ missing.h
@@ -1,9 +1,29 @@
+/*
+   +----------------------------------------------------------------------+
+   | PECL :: http                                                         |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.0 of the PHP license, that  |
+   | is bundled with this package in the file LICENSE, and is available   |
+   | through the world-wide-web at http://www.php.net/license/3_0.txt.    |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2004-2005 Michael Wallner <mike@php.net>               |
+   +----------------------------------------------------------------------+
+*/
+
+/* $Id$ */
 
 #ifndef PHP_HTTP_MISSING
 #define PHP_HTTP_MISSING
 
+#include "php_version.h"
+
+#if (PHP_MAJOR_VERSION == 5) && (PHP_MINOR_VERSION == 0)
 extern int zend_declare_property_double(zend_class_entry *ce, char *name, int name_length, double value, int access_type TSRMLS_DC);
 extern void zend_update_property_double(zend_class_entry *scope, zval *object, char *name, int name_length, double value TSRMLS_DC);
+#endif
 
 #endif
 
