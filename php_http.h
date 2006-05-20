2763e49af54ef5a317337457a993476c18cb4bfb
Michael Wallner <mike@php.net>
2006-05-20 09:46:16 +0000

    - bump version

--- php_http.h
+++ php_http.h
@@ -15,7 +15,7 @@
 #ifndef PHP_EXT_HTTP_H
 #define PHP_EXT_HTTP_H
 
-#define PHP_EXT_HTTP_VERSION "1.0.0RC4"
+#define PHP_EXT_HTTP_VERSION "1.0.0RC5-dev"
 
 #ifdef HAVE_CONFIG_H
 #	include "config.h"
