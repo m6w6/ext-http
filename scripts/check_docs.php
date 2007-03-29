9d41facfe3c41db910df23a6c4e2bbdb702077aa
Michael Wallner <mike@php.net>
2007-03-29 15:34:38 +0000

    - check all class members

--- scripts/check_docs.php
+++ scripts/check_docs.php
@@ -4,7 +4,7 @@
 
 function re($file, $re, &$m=NULL) {
 	global $doc;
-	return preg_match($re, file_get_contents("$doc/$file"), $m);
+	return file_exists("$doc/$file") ? preg_match($re, file_get_contents("$doc/$file"), $m) : 0;
 }
 function fe($file) {
 	global $doc;
@@ -38,12 +38,20 @@ foreach ($ext->getFunctions() as $func) {
 }
 printf("\n");
 
-printf("Undocumented classes/methods:\n");
+printf("Undocumented classes/members:\n");
 foreach ($ext->getClasses() as $class) {
 	 if (substr($class->getName(), -strlen("Exception")) === "Exception") continue;
 	/* @var $class ReflectionClass */
 	fg(sprintf("%s.xml", $class->getName())) or printf(" %s\n", $class->getName());
+	foreach ($class->getConstants() as $name => $tmp) {
+		re($class->getName().".xml", "#>$name<#") or printf("\t%s::%s (%s)\n", $class->getName(), $name, $tmp);
+	}
+	foreach ($class->getProperties() as $prop) {
+		// blatant guess
+		re($class->getName().".xml", "#>{$prop->getName()}<#") or printf("\t%s::$%s\n", $class->getName(), $prop->getName());
+	}
 	foreach ($class->getMethods() as $meth) {
+		/* @var $meth ReflectionMethod */
 		fg(sprintf("%s/%s.xml", $class->getName(), strtr(trim($meth->getName(),'_'),'_','-'))) or printf("\t%s::%s()\n", $class->getName(), $meth->getName());
 	}
 	printf("\n");
