a8ac1f41f21043ec7fed928821255fb4ea5c2c62
Michael Wallner <mike@php.net>
2007-09-28 07:04:27 +0000

    - AC found the header if HTTP_HAVE_[OPENSSL|GNUTLS] is defined

--- php_http_request_int.h
+++ php_http_request_int.h
@@ -18,25 +18,11 @@
 #		include <openssl/crypto.h>
 #	else /* !PHP_WIN32 */
 #		if defined(HTTP_HAVE_OPENSSL)
-#			if defined(HAVE_OPENSSL_CRYPTO_H)
-#				define HTTP_NEED_OPENSSL_TSL
-#				include <openssl/crypto.h>
-#			else
-#				warning \
-					"libcurl was compiled with OpenSSL support, but configure could not find " \
-					"openssl/crypto.h; thus no SSL crypto locking callbacks will be set, which may " \
-					"cause random crashes on SSL requests"
-#			endif
+#			define HTTP_NEED_OPENSSL_TSL
+#			include <openssl/crypto.h>
 #		elif defined(HTTP_HAVE_GNUTLS)
-#			if defined(HAVE_GCRYPT_H)
-#				define HTTP_NEED_GNUTLS_TSL
-#				include <gcrypt.h>
-#			else
-#				warning \
-					"libcurl was compiled with GnuTLS support, but configure could not find " \
-					"gcrypt.h; thus no SSL crypto locking callbacks will be set, which may " \
-					"cause random crashes on SSL requests"
-#			endif
+#			define HTTP_NEED_GNUTLS_TSL
+#			include <gcrypt.h>
 #		else
 #			warning \
 				"libcurl was compiled with SSL support, but configure could not determine which" \
