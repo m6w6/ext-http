a9b5cbfc56a4fcb52e8cf4d353a8b64a8b9acbb2
Michael Wallner <mike@php.net>
2015-09-07 11:32:40 +0200

    add "path_as_is" request option (libcurl >=7.42)

--- php_http_client_curl.c
+++ php_http_client_curl.c
@@ -1212,6 +1212,11 @@ static void php_http_curle_options_init(php_http_options_t *registry TSRMLS_DC)
 {
 	php_http_option_t *opt;
 
+	/* url options */
+#if PHP_HTTP_CURL_VERSION(7,42,0)
+	php_http_option_register(registry, ZEND_STRL("path_as_is"), CURLOPT_PATH_AS_IS, IS_BOOL);
+#endif
+
 	/* proxy */
 	if ((opt = php_http_option_register(registry, ZEND_STRL("proxyhost"), CURLOPT_PROXY, IS_STRING))) {
 		opt->flags |= PHP_HTTP_CURLE_OPTION_CHECK_STRLEN;
@@ -1511,6 +1516,7 @@ static void php_http_curle_options_init(php_http_options_t *registry TSRMLS_DC)
 		}
 #endif
 #if PHP_HTTP_CURL_VERSION(7,39,0)
+		/* FIXME: see http://curl.haxx.se/libcurl/c/CURLOPT_PINNEDPUBLICKEY.html#AVAILABILITY */
 		if ((opt = php_http_option_register(registry, ZEND_STRL("pinned_publickey"), CURLOPT_PINNEDPUBLICKEY, IS_STRING))) {
 			opt->flags |= PHP_HTTP_CURLE_OPTION_CHECK_STRLEN;
 			opt->flags |= PHP_HTTP_CURLE_OPTION_CHECK_BASEDIR;
