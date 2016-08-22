78b58967b4befb4118572312f82f63d77e102b41
Michael Wallner <mike@php.net>
2016-08-22 15:50:10 +0200

    prepare 2.6.0beta1

--- php_http.h
+++ php_http.h
@@ -13,7 +13,7 @@
 #ifndef PHP_EXT_HTTP_H
 #define PHP_EXT_HTTP_H
 
-#define PHP_PECL_HTTP_VERSION "2.6.0dev"
+#define PHP_PECL_HTTP_VERSION "2.6.0beta1"
 
 extern zend_module_entry http_module_entry;
 #define phpext_http_ptr &http_module_entry
