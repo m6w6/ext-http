74984d42a8b71fefc6f3400e0a569f778a0f101e
Remi Collet <remi@remirepo.net>
2024-07-09 15:33:05 +0200

    Fix header name for 8.4

--- src/php_http_misc.c
+++ src/php_http_misc.c
@@ -12,7 +12,11 @@
 
 #include "php_http_api.h"
 
+#if PHP_VERSION_ID < 80400
 #include "ext/standard/php_lcg.h"
+#else
+#include "ext/random/php_random.h"
+#endif
 #include "zend_exceptions.h"
 
 /* SLEEP */
